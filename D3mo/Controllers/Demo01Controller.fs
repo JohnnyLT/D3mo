6846d1645fa01b3dedacbb42ed8926b47e2de309
John Trujillo <john.l.trujillo@gmail.com>
2015-06-25 07:24:59 -0500

    Stand up basic ASP.NET project with D3

---
+++ D3mo/Controllers/Demo01Controller.fs
@@ -0,0 +1,13 @@
+﻿namespace D3mo.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Web
+open System.Web.Mvc
+open System.Web.Mvc.Ajax
+
+type Demo01Controller() =
+    inherit Controller()
+    member this.Index () = this.View()
+
---
+++ D3mo/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Inherits="D3mo.Global" %>
