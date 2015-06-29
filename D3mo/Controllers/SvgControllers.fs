60970f66fef1019513a77879d78b7baae2538463
John Trujillo <john.l.trujillo@gmail.com>
2015-06-28 22:25:15 -0500

    SVG circles

---
+++ D3mo/Controllers/SvgControllers.fs
@@ -0,0 +1,12 @@
+﻿namespace D3mo.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Web
+open System.Web.Mvc
+open System.Web.Mvc.Ajax
+
+type BasicSvgController() =
+    inherit Controller()
+    member this.Index () = this.View()
