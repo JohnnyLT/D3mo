cdcc660f734bd99bf19414da8b30d92fa22629e6
John Trujillo <john.l.trujillo@gmail.com>
2015-07-12 22:14:16 -0500

    Add LineCharts controller and viewer

---
+++ D3mo/Controllers/LineChartsController.fs
@@ -0,0 +1,13 @@
+﻿namespace D3mo.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Web
+open System.Web.Mvc
+open System.Web.Mvc.Ajax
+
+type LineChartsController() =
+    inherit Controller()
+    member this.Index () = this.View()
+
